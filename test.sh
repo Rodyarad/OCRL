8f1fa7ad59ea2160631f15e092c54c7026a0f296
Rodyarad <r.vakhitov2018@gmail.com>
2026-05-04 19:50:30 +0300

    another small fixes

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ export CUDA_LAUNCH_BLOCKING=1
 export PYTHONFAULTHANDLER=1
 export PYTHONUNBUFFERED=1
 
-#python train_sb3.py ocr=slotcontrast pooling=transformer sb3=ppo sb3_acnet=mlp env=vizdoom_sc env.checkpoint_path=/pretrained_encoders/slotcontrast_vizdoom.ckpt max_steps=10000 eval.freq=10 wandb.offline=True
-#python train_sb3.py ocr=slotcontrast pooling=transformer sb3=ppo sb3_acnet=mlp env=maniskill_sc env.checkpoint_path=slotcontrast_maniskill.ckpt max_steps=10000 eval.freq=10 wandb.offline=True
-python train_sb3.py ocr=dinosaur pooling=transformer sb3=ppo sb3_acnet=mlp env=robosuite_dino max_steps=10000 eval.freq=10 wandb.offline=True
+#python train_sb3.py ocr=slotcontrast pooling=transformer sb3=ppo sb3_acnet=mlp env=vizdoom_sc env.checkpoint_path=/pretrained_encoders/slotcontrast_vizdoom.ckpt wandb.offline=True
+#python train_sb3.py ocr=slotcontrast pooling=transformer sb3=ppo sb3_acnet=mlp env=maniskill_sc env.checkpoint_path=slotcontrast_maniskill.ckpt wandb.offline=True
+python train_sb3.py ocr=dinosaur pooling=transformer sb3=ppo sb3_acnet=mlp env=robosuite_dino wandb.offline=True
